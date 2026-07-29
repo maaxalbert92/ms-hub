9f86a1d8a18f959d490f452bc5a6547aa409a377
maaxalbert92 <maax.albert@gmail.com>
2026-07-29 10:30:20 -0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -31,9 +31,9 @@ const systems = [
 
   {name:'Engenharia Elétrica', url:'https://fs-educ.sempreser.com.br/adfs/ls/', category:'Faculdade', desc:'Portal acadêmico da Engenharia Elétrica.', icon:'🎓', favorite:false, tags:['Faculdade']},
   
-  {name:'PFAC Portal', url:'https://pfac-portal.vercel.app/login',  category:'Operação',  desc:'Acesso ao portal operacional do PFAC.',  icon:'🧭',  favorite:true,  tags:['PFAC','Portal','Operação']},
+  {name:'ADTEC - Portal PFAC', url:'https://pfac-portal.vercel.app/login',  category:'Operação',  desc:'Acesso ao portal operacional do PFAC.',  icon:'🧭',  favorite:true,  tags:['PFAC','Portal','Operação']},
   
-  {name:'Cadastro de Estações - CIC',  url:'https://carmoenergy.sharepoint.com/sites/CIC-CentroIntegradodeControle/Lists/Cadastro%20de%20Estaes/AllItems.aspx',  category:'Documentos',  desc:'Cadastro das estações utilizado nos controles e aplicativos do CIC.',  icon:'🏭',  favorite:true,  tags:['Estações','CIC','SharePoint','Cadastro']},
+  {name:'Cadastro de Estações - CIC',  url:'https://carmoenergy.sharepoint.com/sites/CIC-CentroIntegradodeControle',  category:'Documentos',  desc:'Portal CIC no SharePoint',  icon:'🏭',  favorite:true,  tags:['CIC','SharePoint']},
   
   {name:'Power Apps',  url:'https://make.powerapps.com/environments/Default-ebba8479-b898-4081-a0de-dcaad02e0367/home',  category:'Desenvolvimento',  desc:'Criação, edição e gerenciamento dos aplicativos Power Apps.',  icon:'🧩',  favorite:true,  tags:['Power Apps','Microsoft','Aplicativos']}
 ];
